c89379a57868b18d462b48dcd0155bfd090d1259
pratheek shetty <pratheekshetty7>
2022-04-15 12:41:45 +0530

    controller structure

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -12,16 +12,15 @@ module.exports = {
         if (!user_id) {
             return res.status(400).send({ error: true, message: 'Please provide user_id' });
         }
-        userModel.userWithId(req,res,user_id)
-        
+        userModel.userWithId(req, res, user_id)
     },
     newUser: (req, res) => {
         let user = req.body.user;
         if (!user) {
             return res.status(400).send({ error: true, message: 'Please provide user', success: false });
         }
-        userModel.newUSer(req,res,user)
-        
+        userModel.newUSer(req, res, user)
+
     },
     editUser: (req, res) => {
 
@@ -30,8 +29,8 @@ module.exports = {
         if (!user_id || !user) {
             return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
         }
-        userModel.editUSer(req,res,user_id,user);
-     
+        userModel.editUSer(req, res, user_id, user);
+
     },
     deleteUser: (req, res) => {
         let user_id = req.body.user_id;
@@ -39,6 +38,6 @@ module.exports = {
         if (!user_id) {
             return res.status(400).send({ error: true, message: 'Please provide user_id' });
         }
-     userModel.deleteUSer(req,res,user_id)
+        userModel.deleteUSer(req, res, user_id)
     }
 }
